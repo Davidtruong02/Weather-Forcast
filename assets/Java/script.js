d6ef969e24151632e270a7f0c879850fda65c82a
David Truong <Davidtruong134@gmail.com>
2024-01-06 13:14:21 -0600

    deleted tested code trying new ones

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -1,4 +1,4 @@
 function getWeather() {
-    let city = document.getElementById('city').value;
-    window.location.href='weather.html?city=${city}';
+    const city = document.getElementById('city').value;
+    window.location.href = `weather.html?city=${city}`;
 }
