d0f89891c50021c6484a6545ffdd5614f6377061
David Truong <Davidtruong134@gmail.com>
2024-01-07 21:24:05 -0600

    more trial and error codes

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -1,3 +1,10 @@
 let apiKey = 'f1820185a54de142b8e99d9732f66c85';
-let apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
-let
+let openWeatherUrl = 'api.openweathermap.org/data/2.5/weather?q';
+let forecastUrl = 'api.openweathermap.org/data/2.5/forecast?lat=';
+let formEl = $('#search-city');
+let currentDate = moment().format('DD/MM/YYYY');
+let forecastEl = $('.forcast');
+let historyEl = $('#history');
+let cityEl = $('#city');
+
+
